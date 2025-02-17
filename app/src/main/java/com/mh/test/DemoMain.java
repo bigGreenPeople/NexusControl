67ae1b6cc4895e00579acbaa3ed206d2ed068167
bigRedPeoPle <1273596620@qq.com>
2025-02-17 23:01:20 +0800

    方法抽取

--- app/src/main/java/com/mh/test/DemoMain.java
+++ app/src/main/java/com/mh/test/DemoMain.java
@@ -1,5 +1,7 @@
 package com.mh.test;
 
+import android.util.Log;
+
 import com.shark.SuperModule;
 import com.shark.input.InputManager;
 
@@ -7,6 +9,7 @@ public class DemoMain extends SuperModule {
     @Override
     public void main(ClassLoader classLoader, String processName, String packageName) {
         trackActivityOnResume(classLoader);
+        Log.i(TAG, "main: DemoMain");
         InputManager inputManager = InputManager.getInstance();
 //        inputManager.inputText("Hello NexusControl!!");
         InputManager.getInstance().swipe(241, 97, 235, 541);
